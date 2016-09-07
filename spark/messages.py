365d17256663649bbf08b4d752ceb56bc5718c92
kecorbin <kecorbin@cisco.com>
2016-09-07 16:05:05 -0500

    add support to get msg by ID

--- spark/messages.py
+++ spark/messages.py
@@ -50,3 +50,22 @@ class Message(object):
     @classmethod
     def url(cls):
         return '/messages'
+
+    @classmethod
+    def from_json(cls, obj):
+        if isinstance(obj, dict):
+            obj = cls(attributes=obj)
+        elif isinstance(obj, (str, unicode)):
+            obj = cls(attributes=json.loads(obj))
+        else:
+            raise TypeError('Data must be str or dict')
+        return obj
+
+    @classmethod
+    def get(cls, session, msgid=None):
+        url = '/messages/{}'.format(msgid)
+        resp = session.get(url)
+
+        obj = cls.from_json(resp.json())
+        return obj
+
